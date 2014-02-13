dd2aa3d63faad7b6b3c7485990af213ee1e01748
Olwe <Olwe@ZINSE-PC>
2014-02-13 12:07:20 +0100

    Crud contact généré

---
+++ app/cache/dev/twig/1c/96/0d31b69d1789d03e0739c544b5f359e168585308aa83835fb7c04e2a4447.php
@@ -0,0 +1,65 @@
+<?php
+
+/* YnovLabsBundle:Contact:new.html.twig */
+class __TwigTemplate_1c960d31b69d1789d03e0739c544b5f359e168585308aa83835fb7c04e2a4447 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "<h1>Contact creation</h1>
+
+    ";
+        // line 6
+        echo         $this->env->getExtension('form')->renderer->renderBlock($this->getContext($context, "form"), 'form');
+        echo "
+
+        <ul class=\"record_actions\">
+    <li>
+        <a href=\"";
+        // line 10
+        echo $this->env->getExtension('routing')->getPath("contact");
+        echo "\">
+            Back to the list
+        </a>
+    </li>
+</ul>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "YnovLabsBundle:Contact:new.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  42 => 10,  35 => 6,  31 => 4,  28 => 3,);
+    }
+}
